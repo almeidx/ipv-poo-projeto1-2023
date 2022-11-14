3f5031db757fb832eafccdadd4aad9b37219ccfd
Gonçalo Fonseca <105716160+goncalofonsecaa@users.noreply.github.com>
2022-11-14 15:18:29 +0000

    Gonçalofonseca (#7)
    * sensores atualizados
    
    * classe sensor (atualizados)

--- Sensor.h
+++ Sensor.h
@@ -16,4 +16,56 @@ public:
 	~Sensor();
 };
 
+class SensorLuz : public Sensor
+{
+public:
+	SensorLuz(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~SensorLuz();
+};
+
+class SHumidade : public Sensor
+{
+public:
+	SHumidade(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~SHumidade();
+};
+
+
+class SFumo : public Sensor
+{
+public:
+	SFumo(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~SFumo();
+};
+
+class SMisseis : public Sensor
+{
+
+public:
+   	 SMisseis(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~SMisseis();
+};
+
+class SFogo : public Sensor
+{
+
+public:
+   	 SFogo(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~SFogo();
+};
+
+class STemperatura : public Sensor
+{
+
+public:
+   	 STemperatura(string id, string marca, float valor, float probabilidade_avaria) : Sensor(id, marca, valor, probabilidade_avaria) {}
+	~STemperatura();
+};
+
+
+
+
+
+
+
 #endif // SENSOR_H
