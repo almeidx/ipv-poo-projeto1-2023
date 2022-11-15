ae1e0fe702dba44d9111b6bf06ac1278577eca33
jpedro058 <98242911+jpedro058@users.noreply.github.com>
2022-11-15 09:35:31 +0000

    distancia entre pontos (#12)

--- Ponto.h
+++ Ponto.h
@@ -1,6 +1,8 @@
 #ifndef PONTO_H
 #define PONTO_H
 
+#include <cmath>
+
 class Pair {
 	int x, y;
 
@@ -21,9 +23,7 @@ public:
 	Ponto(int x, int y) : Pair(x, y) {}
 	~Ponto() {}
 
-	float Distancia(Ponto p) {
-		// TODO: Calcular distância
-	}
+	float Distancia(Ponto p) { return sqrt(pow(p.Get_X() - Get_X(), 2) + pow(p.Get_Y() - Get_Y(), 2)); }
 };
 
 #endif // PONTO_H
